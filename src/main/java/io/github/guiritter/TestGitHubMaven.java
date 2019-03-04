bc47015d44d5f15e4868e8824f071270c1bc9150
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2019-03-04 15:32:47 -0300

    initial commit

---
+++ src/main/java/io/github/guiritter/TestGitHubMaven.java
@@ -0,0 +1,16 @@
+package io.github.guiritter;
+
+/**
+ * Hello world class!
+ */
+public final class TestGitHubMaven {
+
+    /**
+     * Hello world string!
+     */
+    public static final String HELLO_WORLD = "Hello World!";
+
+    public static void main(String[] args) {
+        System.out.println(HELLO_WORLD);
+    }
+}
